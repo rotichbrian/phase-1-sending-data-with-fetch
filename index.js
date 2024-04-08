819df33a092be82d4282b2ef46965ae8c6ee3360
Brian Kipkorir <rotichbrian891@gmail.com>
2024-04-08 14:02:10 +0300

    asiignment completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+const submitData = (inputName, inputEmail) => {
+
+    const formData = {
+        name: inputName,
+        email: inputEmail,
+      };
+
+    const renderID = (i) => {     
+      let h1 = document.createElement('h1')
+      h1.textContent = i
+      document.body.appendChild(h1)
+    }
+    
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+          renderID(object.id)
+        })
+        .catch(function (error) {         
+          console.log(error.message);
+          let message = 'Unauthorized Access'
+          let h2 = document.createElement('h2')
+          h2.textContent = message
+          document.body.appendChild(h2)
+        });
+  }     
+
